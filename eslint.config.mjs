f1d527225b82ce582034c0fe24a6ae50e9e4e4cc
nekko1119 <2772307+nekko1119@users.noreply.github.com>
2026-05-05 19:24:08 +0900

    lintの設定を改善して、エディタでinterfaceなどの警告が出ないようにする

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,24 @@ import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
+import astroParser from "astro-eslint-parser";
+import typescriptEslintParser from "@typescript-eslint/parser";
 
 export default defineConfig([
+  {
+    ignores: [".astro/", "dist/", "node_modules/"],
+  },
   js.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.recommendedTypeChecked,
   ...eslintPluginAstro.configs.recommended,
   {
+    files: ["*.astro"],
     languageOptions: {
+      parser: astroParser,
       parserOptions: {
-        projectService: true,
-      }
-    }
+        parser: typescriptEslintParser,
+        extraFileExtensions: [".astro"],
+      },
+    },
   },
 ]);
