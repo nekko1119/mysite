44870f08d646fb3e6bd6aec48313b39da943f292
nekko1119 <2772307+nekko1119@users.noreply.github.com>
2026-05-07 01:04:19 +0900

    スクリーンショットファイル名にplatformを消す、テストの安定化

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -3,16 +3,25 @@ import { test, expect } from "@playwright/test";
 [
   {
     path: "/",
+    name: "index.png",
   },
   {
     path: "/tech",
+    name: "tech.png",
   },
   {
     path: "/hobby",
+    name: "hobby.png",
   },
-].forEach(({ path }) => {
+].forEach(({ path, name }) => {
   test(`Snapshot with "${path}"`, async ({ page }) => {
     await page.goto(path, { waitUntil: "domcontentloaded" });
-    await expect(page).toHaveScreenshot({ fullPage: true });
+    if (path === "/") {
+      // スナップショット時にアイコン画像のロードが不安定なので待つ
+      const image = page.getByAltText("nekko1119のアイコン");
+      await expect(image).toBeVisible();
+      await expect(image).not.toHaveJSProperty("naturalWidth", 0);
+    }
+    await expect(page).toHaveScreenshot(name, { fullPage: true });
   });
 });
