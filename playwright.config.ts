ac6bbaf851a43ed37716e9ba690af647a2b4b3a9
nekko1119 <2772307+nekko1119@users.noreply.github.com>
2026-05-07 00:03:40 +0900

    重複しているprojectを削除

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,10 +41,6 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
