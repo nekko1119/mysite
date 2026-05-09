3aff9e0c5b68b2ecd9c3affd6708531284ff5f0a
nekko1119 <2772307+nekko1119@users.noreply.github.com>
2026-05-09 20:25:51 +0900

    axe-coreを導入しテストに追加

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
         ["json", { outputFile: "playwright-results.json" }],
         ["html", { open: "never" }],
       ]
-    : [["list"]],
+    : [["dot"], ["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, type Page } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
-[
+const paths = [
   {
     path: "/",
     name: "index.png",
@@ -13,15 +14,33 @@ import { test, expect } from "@playwright/test";
     path: "/hobby",
     name: "hobby.png",
   },
-].forEach(({ path, name }) => {
-  test(`Snapshot with "${path}"`, async ({ page }) => {
+];
+
+// アイコン画像のロードが不安定なので待つ
+async function awaitIconVisible({ page }: { page: Page }) {
+  const image = page.getByAltText("nekko1119のアイコン");
+  await expect(image).toBeVisible();
+  await expect(image).not.toHaveJSProperty("naturalWidth", 0);
+}
+
+paths.forEach(({ path, name }) => {
+  test(`Snapshot with "${path}"`, { tag: ["@vrt"] }, async ({ page }) => {
     await page.goto(path, { waitUntil: "domcontentloaded" });
     if (path === "/") {
-      // スナップショット時にアイコン画像のロードが不安定なので待つ
-      const image = page.getByAltText("nekko1119のアイコン");
-      await expect(image).toBeVisible();
-      await expect(image).not.toHaveJSProperty("naturalWidth", 0);
+      // アイコン画像のロードが不安定なので待つ
+      await awaitIconVisible({ page });
     }
     await expect(page).toHaveScreenshot(name, { fullPage: true });
+  });
+});
+
+paths.forEach(({ path }) => {
+  test(`Axe core with "${path}"`, { tag: ["@axe"] }, async ({ page }) => {
+    await page.goto(path, { waitUntil: "domcontentloaded" });
+    if (path === "/") {
+      await awaitIconVisible({ page });
+    }
+    const axeResults = await new AxeBuilder({ page }).analyze();
+    expect(axeResults.violations).toEqual([]);
   });
 });
